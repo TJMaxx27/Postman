25aefbe24f14a078a5be6152af863c0dfc2cf471
TJMaxx27 <99042166+TJMaxx27@users.noreply.github.com>
2022-07-08 01:27:03 +0400

    Update GET_First.js

--- HW_2/GET_First.js
+++ HW_2/GET_First.js
@@ -1,5 +1,5 @@
-http://162.55.220.72:5005/first
-1. Отправить запрос.
+// http://162.55.220.72:5005/first
+// 1. Отправить запрос.
 // 2. Статус код 200
 pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
